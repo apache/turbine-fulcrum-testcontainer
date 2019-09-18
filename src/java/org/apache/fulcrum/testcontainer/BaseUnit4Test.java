31b03b831bd3424de42a33fb11e11508fe8c4dcd
Jeffery Painter <painter@apache.org>
2019-09-18 01:25:12 +0000

    Update junit and jupiter dependencies, source cleanup for Turbine compliance
    git-svn-id: https://svn.apache.org/repos/asf/turbine/fulcrum/trunk/testcontainer@1867084 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fulcrum/testcontainer/BaseUnit4Test.java
+++ src/java/org/apache/fulcrum/testcontainer/BaseUnit4Test.java
@@ -1,20 +1,5 @@
 package org.apache.fulcrum.testcontainer;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Vector;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -33,6 +18,22 @@ import javax.servlet.http.HttpSession;
  * specific language governing permissions and limitations
  * under the License.
  */
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Vector;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.apache.avalon.framework.component.ComponentException;
 import org.apache.avalon.framework.logger.ConsoleLogger;
 import org.junit.After;
@@ -40,260 +41,258 @@ import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 /**
- * Alternative Base class to {@link BaseUnitTest} for component tests. 
+ * Alternative Base class to {@link BaseUnitTest} for component tests.
  * 
- * This version doesn't load the container until the
- * first request for a component. This allows the tester to populate the configurationFileName and
+ * This version doesn't load the container until the first request for a
+ * component. This allows the tester to populate the configurationFileName and
  * roleFileName, possible one per test.
  * 
  * JUnit 4 Version of BaseUnitTest class.
  * 
- * @see BaseUnitTest 
+ * @see BaseUnitTest
  *
  * @author <a href="mailto:epugh@upstate.com">Eric Pugh</a>
  * @author <a href="mailto:quintonm@bellsouth.net">Quinton McCombs</a>
  * @version $Id$
  */
 
-public class BaseUnit4Test
-{
-    public static final String CONTAINER_ECM = "CONTAINER_ECM";
-    public static final String CONTAINER_YAAFI = "CONTAINER_YAAFI";
+public class BaseUnit4Test {
+	public static final String CONTAINER_ECM = "CONTAINER_ECM";
+	public static final String CONTAINER_YAAFI = "CONTAINER_YAAFI";
 
-    /** Key used in the context for defining the application root */
-    public static final String COMPONENT_APP_ROOT = Container.COMPONENT_APP_ROOT;
+	/** Key used in the context for defining the application root */
+	public static final String COMPONENT_APP_ROOT = Container.COMPONENT_APP_ROOT;
 
-    /** Pick the default container to be YAAFI **/
-    private String containerType = CONTAINER_YAAFI;
+	/** Pick the default container to be YAAFI **/
+	private String containerType = CONTAINER_YAAFI;
 
-    /** Use INFO for ConsoleLogger */
-    public static final int defaultLogLevel = ConsoleLogger.LEVEL_INFO;
+	/** Use INFO for ConsoleLogger */
+	public static final int defaultLogLevel = ConsoleLogger.LEVEL_INFO;
 
-    /** Container for the components */
-    private Container container;
+	/** Container for the components */
+	private Container container;
 
-    /** Setup our default configurationFileName */
-    private String configurationFileName = "src/test/TestComponentConfig.xml";
+	/** Setup our default configurationFileName */
+	private String configurationFileName = "src/test/TestComponentConfig.xml";
 
-    /** Setup our default roleFileName */
-    private String roleFileName = "src/test/TestRoleConfig.xml";
+	/** Setup our default roleFileName */
+	private String roleFileName = "src/test/TestRoleConfig.xml";
 
-    /** Setup our default parameterFileName */
-    private String parameterFileName = null;
+	/** Setup our default parameterFileName */
+	private String parameterFileName = null;
 
-    /** Set the log level (only works for YAAFI container) */
-    private int logLevel = defaultLogLevel;
+	/** Set the log level (only works for YAAFI container) */
+	private int logLevel = defaultLogLevel;
 
-    /** Hash map to store attributes for the test **/
-    public Map<String,Object> attributes = new HashMap<String,Object>();
-    
-    /** set the Max inactive interval **/
-    public int maxInactiveInterval = 0;
-    
-    /**
-     * Gets the configuration file name for the container should use for this test. By default it
-     * is src/test/TestComponentConfig.
-     * 
-     * @param configurationFileName the location of the config file
-     */
-    protected void setConfigurationFileName(String configurationFileName)
-    {
-        this.configurationFileName = configurationFileName;
-    }
+	/** Hash map to store attributes for the test **/
+	public Map<String, Object> attributes = new HashMap<String, Object>();
 
-    /**
-     * Override the role file name for the container should use for this test. By default it is
-     * src/test/TestRoleConfig.
-     * 
-     * @param roleFileName location of the role file
-     */
-    protected void setRoleFileName(String roleFileName)
-    {
-        this.roleFileName = roleFileName;
-    }
+	/** set the Max inactive interval **/
+	public int maxInactiveInterval = 0;
 
-    /**
-     * Set the console logger level
-     * 
-     * @see org.apache.avalon.framework.logger.ConsoleLogger for debugging levels
-     * @param logLevel set valid logging level
-     */
-    protected void setLogLevel(int logLevel) {
-        this.logLevel = logLevel;
-    }
+	/**
+	 * Gets the configuration file name for the container should use for this test.
+	 * By default it is src/test/TestComponentConfig.
+	 * 
+	 * @param configurationFileName the location of the config file
+	 */
+	protected void setConfigurationFileName(String configurationFileName) 
+	{
+		this.configurationFileName = configurationFileName;
+	}
 
-    /**
-     * Constructor for test.
-     */
-    public BaseUnit4Test()
-    {
-    }
+	/**
+	 * Override the role file name for the container should use for this test. By
+	 * default it is src/test/TestRoleConfig.
+	 * 
+	 * @param roleFileName location of the role file
+	 */
+	protected void setRoleFileName(String roleFileName) 
+	{
+		this.roleFileName = roleFileName;
+	}
 
-    /**
-     * Clean up after each test is run.
-     */
-    @After
-    public void tearDown()
-    {
-        if (container != null)
-        {
-            container.dispose();
-        }
-        container = null;
-    }
+	/**
+	 * Set the console logger level
+	 * 
+	 * @see org.apache.avalon.framework.logger.ConsoleLogger for debugging levels
+	 * @param logLevel set valid logging level
+	 */
+	protected void setLogLevel(int logLevel) 
+	{
+		this.logLevel = logLevel;
+	}
 
-    /**
-     * Gets the configuration file name for the container should use for this test.
-     *
-     * @return The filename of the configuration file
-     */
-    protected String getConfigurationFileName()
-    {
-        return configurationFileName;
-    }
+	/**
+	 * Constructor for test.
+	 */
+	public BaseUnit4Test() 
+	{
+	}
 
-    /**
-     * Gets the role file name for the container should use for this test.
-     *
-     * @return The filename of the role configuration file
-     */
-    protected String getRoleFileName()
-    {
-        return roleFileName;
-    }
+	/**
+	 * Clean up after each test is run.
+	 */
+	@After
+	public void tearDown() 
+	{
+		if (container != null) {
+			container.dispose();
+		}
+		container = null;
+	}
 
-    /**
-     * Gets the parameter file name for the container should use for this test.
-     *
-     * @return The filename of the role configuration file
-     */
-    protected String getParameterFileName()
-    {
-        return parameterFileName;
-    }
+	/**
+	 * Gets the configuration file name for the container should use for this test.
+	 *
+	 * @return The filename of the configuration file
+	 */
+	protected String getConfigurationFileName() 
+	{
+		return configurationFileName;
+	}
 
-    /**
-     * Returns an instance of the named component. 
-     * This method will also start the container if it
-     * has not been started already
-     *
-     * @param roleName Name of the role the component fills.
-     * @return instance of the component
-     * @throws ComponentException generic exception
-     */
-    protected Object lookup(String roleName) throws ComponentException
-    {
-        if (container == null)
-        {
-            if(containerType.equals(CONTAINER_ECM)){
-                container = new ECMContainer();
-            }
-            else {
-                container = new YAAFIContainer(logLevel);
-            }
-            container.startup(getConfigurationFileName(), getRoleFileName(),getParameterFileName());
-        }
-        return container.lookup(roleName);
-    }
+	/**
+	 * Gets the role file name for the container should use for this test.
+	 *
+	 * @return The filename of the role configuration file
+	 */
+	protected String getRoleFileName() 
+	{
+		return roleFileName;
+	}
 
-    /**
-     * Releases the component.
-     *
-     * @param component component to be released
-     */
-    protected void release(Object component)
-    {
-        if (container != null)
-        {
-            container.release(component);
-        }
-    }
-    
+	/**
+	 * Gets the parameter file name for the container should use for this test.
+	 *
+	 * @return The filename of the role configuration file
+	 */
+	protected String getParameterFileName() 
+	{
+		return parameterFileName;
+	}
 
-    /**
-     * Get a mock requestion 
-     *
-     * @return HttpServletRequest a mock servlet request
-     */
-    protected HttpServletRequest getMockRequest()
-    {
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpSession session = mock(HttpSession.class);
+	/**
+	 * Returns an instance of the named component. This method will also start the
+	 * container if it has not been started already
+	 *
+	 * @param roleName Name of the role the component fills.
+	 * @return instance of the component
+	 * @throws ComponentException generic exception
+	 */
+	protected Object lookup(String roleName) throws ComponentException 
+	{
+		if (container == null) 
+		{
+			if (containerType.equals(CONTAINER_ECM)) 
+			{
+				container = new ECMContainer();
+			} 
+			else 
+			{
+				container = new YAAFIContainer(logLevel);
+			}
+			container.startup(getConfigurationFileName(), getRoleFileName(), getParameterFileName());
+		}
+		return container.lookup(roleName);
+	}
 
-        doAnswer(new Answer<Object>()
-        {
-            @Override
-            public Object answer(InvocationOnMock invocation) throws Throwable
-            {
-                String key = (String) invocation.getArguments()[0];
-                return attributes.get(key);
-            }
-        }).when(session).getAttribute(anyString());
+	/**
+	 * Releases the component.
+	 *
+	 * @param component component to be released
+	 */
+	protected void release(Object component) 
+	{
+		if (container != null) 
+		{
+			container.release(component);
+		}
+	}
 
-        doAnswer(new Answer<Object>()
-        {
-            @Override
-            public Object answer(InvocationOnMock invocation) throws Throwable
-            {
-                String key = (String) invocation.getArguments()[0];
-                Object value = invocation.getArguments()[1];
-                attributes.put(key, value);
-                return null;
-            }
-        }).when(session).setAttribute(anyString(), any());
+	/**
+	 * Get a mock requestion
+	 *
+	 * @return HttpServletRequest a mock servlet request
+	 */
+	protected HttpServletRequest getMockRequest() 
+	{
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpSession session = mock(HttpSession.class);
 
-        when(session.getMaxInactiveInterval()).thenReturn(maxInactiveInterval);
+		doAnswer(new Answer<Object>() 
+		{
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable 
+			{
+				String key = (String) invocation.getArguments()[0];
+				return attributes.get(key);
+			}
+		}).when(session).getAttribute(anyString());
 
-        doAnswer(new Answer<Integer>()
-        {
-            @Override
-            public Integer answer(InvocationOnMock invocation) throws Throwable
-            {
-                return Integer.valueOf(maxInactiveInterval);
-            }
-        }).when(session).getMaxInactiveInterval();
+		doAnswer(new Answer<Object>() 
+		{
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable 
+			{
+				String key = (String) invocation.getArguments()[0];
+				Object value = invocation.getArguments()[1];
+				attributes.put(key, value);
+				return null;
+			}
+		}).when(session).setAttribute(anyString(), any());
 
-        doAnswer(new Answer<Object>()
-        {
-            @Override
-            public Object answer(InvocationOnMock invocation) throws Throwable
-            {
-                Integer value = (Integer) invocation.getArguments()[0];
-                maxInactiveInterval = value.intValue();
-                return null;
-            }
-        }).when(session).setMaxInactiveInterval(anyInt());
+		when(session.getMaxInactiveInterval()).thenReturn(maxInactiveInterval);
 
-        when(session.isNew()).thenReturn(true);
-        when(request.getSession()).thenReturn(session);
+		doAnswer(new Answer<Integer>() 
+		{
+			@Override
+			public Integer answer(InvocationOnMock invocation) throws Throwable {
+				return Integer.valueOf(maxInactiveInterval);
+			}
+		}).when(session).getMaxInactiveInterval();
 
-        when(request.getServerName()).thenReturn("bob");
-        when(request.getProtocol()).thenReturn("http");
-        when(request.getScheme()).thenReturn("scheme");
-        when(request.getPathInfo()).thenReturn("damn");
-        when(request.getServletPath()).thenReturn("damn2");
-        when(request.getContextPath()).thenReturn("wow");
-        when(request.getContentType()).thenReturn("text/html");
+		doAnswer(new Answer<Object>() 
+		{
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable 
+			{
+				Integer value = (Integer) invocation.getArguments()[0];
+				maxInactiveInterval = value.intValue();
+				return null;
+			}
+		}).when(session).setMaxInactiveInterval(anyInt());
 
-        when(request.getCharacterEncoding()).thenReturn("US-ASCII");
-        when(request.getServerPort()).thenReturn(8080);
-        when(request.getLocale()).thenReturn(Locale.US);
+		when(session.isNew()).thenReturn(true);
+		when(request.getSession()).thenReturn(session);
 
-        when(request.getHeader("Content-type")).thenReturn("text/html");
-        when(request.getHeader("Accept-Language")).thenReturn("en-US");
+		when(request.getServerName()).thenReturn("bob");
+		when(request.getProtocol()).thenReturn("http");
+		when(request.getScheme()).thenReturn("scheme");
+		when(request.getPathInfo()).thenReturn("damn");
+		when(request.getServletPath()).thenReturn("damn2");
+		when(request.getContextPath()).thenReturn("wow");
+		when(request.getContentType()).thenReturn("text/html");
 
-        Vector<String> v = new Vector<String>();
-        when(request.getParameterNames()).thenReturn(v.elements());
-        return request;
-    }
+		when(request.getCharacterEncoding()).thenReturn("US-ASCII");
+		when(request.getServerPort()).thenReturn(8080);
+		when(request.getLocale()).thenReturn(Locale.US);
 
-    public String getContainerType()
-    {
-        return containerType;
-    }
+		when(request.getHeader("Content-type")).thenReturn("text/html");
+		when(request.getHeader("Accept-Language")).thenReturn("en-US");
 
-    public void setContainerType( String containerType )
-    {
-        this.containerType = containerType;
-    }
+		Vector<String> v = new Vector<String>();
+		when(request.getParameterNames()).thenReturn(v.elements());
+		return request;
+	}
+
+	public String getContainerType() 
+	{
+		return containerType;
+	}
+
+	public void setContainerType(String containerType) 
+	{
+		this.containerType = containerType;
+	}
 }
--- src/java/org/apache/fulcrum/testcontainer/BaseUnitTest.java
+++ src/java/org/apache/fulcrum/testcontainer/BaseUnitTest.java
@@ -1,4 +1,5 @@
 package org.apache.fulcrum.testcontainer;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -17,15 +18,16 @@ package org.apache.fulcrum.testcontainer;
  * specific language governing permissions and limitations
  * under the License.
  */
-import junit.framework.TestCase;
 
 import org.apache.avalon.framework.component.ComponentException;
 import org.apache.avalon.framework.logger.ConsoleLogger;
 
+import junit.framework.TestCase;
+
 /**
- * Base class for unit tests for components. This version doesn't load the container until the
- * first request for a component. This allows the tester to populate the configurationFileName and
- * roleFileName, possible one per test.
+ * Base class for unit tests for components. This version doesn't load the
+ * container until the first request for a component. This allows the tester to
+ * populate the configurationFileName and roleFileName, possible one per test.
  * 
  * This class uses JUnit 3.
  * 
@@ -35,164 +37,165 @@ import org.apache.avalon.framework.logger.ConsoleLogger;
  * @author <a href="mailto:quintonm@bellsouth.net">Quinton McCombs</a>
  * @version $Id$
  */
-public class BaseUnitTest extends TestCase
-{
-    public static final String CONTAINER_ECM="CONTAINER_ECM";
-    public static final String CONTAINER_YAAFI="CONTAINER_YAAFI";
+public class BaseUnitTest extends TestCase {
+	public static final String CONTAINER_ECM = "CONTAINER_ECM";
+	public static final String CONTAINER_YAAFI = "CONTAINER_YAAFI";
 
-    /** Key used in the context for defining the application root */
-    public static final String COMPONENT_APP_ROOT = Container.COMPONENT_APP_ROOT;
+	/** Key used in the context for defining the application root */
+	public static final String COMPONENT_APP_ROOT = Container.COMPONENT_APP_ROOT;
 
-    /** Pick the default container to be Yaafi **/
-    public static final String containerType = CONTAINER_YAAFI;
+	/** Pick the default container to be Yaafi **/
+	public static final String containerType = CONTAINER_YAAFI;
 
-    /** Use INFO for ConsoleLogger */
-    public static final int defaultLogLevel = ConsoleLogger.LEVEL_INFO;
+	/** Use INFO for ConsoleLogger */
+	public static final int defaultLogLevel = ConsoleLogger.LEVEL_INFO;
 
-    /** Container for the components */
-    private Container container;
-    
-    /** Setup our default configurationFileName */
-    private String configurationFileName = "src/test/TestComponentConfig.xml";
-    
-    /** Setup our default roleFileName */
-    private String roleFileName = "src/test/TestRoleConfig.xml";
-    
-    /** Setup our default parameterFileName */
-    private String parameterFileName = null;
-    
-    /** Set the log level (only works for YAAFI container) */
-    private int logLevel = defaultLogLevel;
+	/** Container for the components */
+	private Container container;
 
-    /**
-	 * Gets the configuration file name for the container should use for this test. By default it
-	 * is src/test/TestComponentConfig.
+	/** Setup our default configurationFileName */
+	private String configurationFileName = "src/test/TestComponentConfig.xml";
+
+	/** Setup our default roleFileName */
+	private String roleFileName = "src/test/TestRoleConfig.xml";
+
+	/** Setup our default parameterFileName */
+	private String parameterFileName = null;
+
+	/** Set the log level (only works for YAAFI container) */
+	private int logLevel = defaultLogLevel;
+
+	/**
+	 * Gets the configuration file name for the container should use for this test.
+	 * By default it is src/test/TestComponentConfig.
+	 * 
 	 * @param configurationFileName the location of the config file
 	 */
-    protected void setConfigurationFileName(String configurationFileName)
-    {
-        this.configurationFileName = configurationFileName;
-    }
+	protected void setConfigurationFileName(String configurationFileName) 
+	{
+		this.configurationFileName = configurationFileName;
+	}
 
-    /**
-	 * Override the role file name for the container should use for this test. By default it is
-	 * src/test/TestRoleConfig.
+	/**
+	 * Override the role file name for the container should use for this test. By
+	 * default it is src/test/TestRoleConfig.
 	 * 
 	 * @param roleFileName location of the role file
 	 */
-    protected void setRoleFileName(String roleFileName)
-    {
-        this.roleFileName = roleFileName;
-    }
+	protected void setRoleFileName(String roleFileName) 
+	{
+		this.roleFileName = roleFileName;
+	}
 
-    /**
-     * Set the console logger level
-     * 
-     * @see org.apache.avalon.framework.logger.ConsoleLogger for debugging levels
-     * @param logLevel set valid logging level
-     */
-    protected void setLogLevel(int logLevel) {
-        this.logLevel = logLevel;
-    }
+	/**
+	 * Set the console logger level
+	 * 
+	 * @see org.apache.avalon.framework.logger.ConsoleLogger for debugging levels
+	 * @param logLevel set valid logging level
+	 */
+	protected void setLogLevel(int logLevel) 
+	{
+		this.logLevel = logLevel;
+	}
 
-    /**
+	/**
 	 * Constructor for test.
 	 *
 	 * @param testName name of the test being executed
 	 */
-    public BaseUnitTest(String testName)
-    {
-        super(testName);
-    }
+	public BaseUnitTest(String testName) 
+	{
+		super(testName);
+	}
 
-    /**
+	/**
 	 * Clean up after each test is run.
 	 */
-    protected void tearDown()
-    {
-        if (container != null)
-        {
-            container.dispose();
-        }
-        container = null;
-    }
+	protected void tearDown() 
+	{
+		if (container != null) 
+		{
+			container.dispose();
+		}
+		container = null;
+	}
 
-    /**
+	/**
 	 * Gets the configuration file name for the container should use for this test.
 	 *
 	 * @return The filename of the configuration file
 	 */
-    protected String getConfigurationFileName()
-    {
-        return configurationFileName;
-    }
+	protected String getConfigurationFileName() 
+	{
+		return configurationFileName;
+	}
 
-    /**
+	/**
 	 * Gets the role file name for the container should use for this test.
 	 *
 	 * @return The filename of the role configuration file
 	 */
-    protected String getRoleFileName()
-    {
-        return roleFileName;
-    }
+	protected String getRoleFileName() 
+	{
+		return roleFileName;
+	}
 
-    /**
-     * Gets the parameter file name for the container should use for this test.
-     *
-     * @return The filename of the role configuration file
-     */
-    protected String getParameterFileName()
-    {
-        return parameterFileName;
-    }
-
-    /**
-     * Returns an instance of the named component. 
-     * This method will also start the container if it
-     * has not been started already
-     *
-     * @param roleName Name of the role the component fills.
-     * @return instance of the component
-     * @throws ComponentException generic exception
+	/**
+	 * Gets the parameter file name for the container should use for this test.
+	 *
+	 * @return The filename of the role configuration file
 	 */
-    protected Object lookup(String roleName) throws ComponentException
-    {
-        if (container == null)
-        {
-            if(containerType.equals(CONTAINER_ECM)){
-                container = new ECMContainer();
-            }
-            else {
-                container = new YAAFIContainer(logLevel);
-            }
-            container.startup(getConfigurationFileName(), getRoleFileName(),getParameterFileName());
-        }
-        return container.lookup(roleName);
-    }
+	protected String getParameterFileName() 
+	{
+		return parameterFileName;
+	}
 
-    /**
-     * Helper method for converting to and from Merlin Unit TestCase.
-     * 
-     * @param roleName the role name to resolve
-     * @return the component matching the role
-     * @throws ComponentException generic exception
-     */
-    protected Object resolve(String roleName) throws ComponentException
-    {
-        return lookup(roleName);
-    }
+	/**
+	 * Returns an instance of the named component. This method will also start the
+	 * container if it has not been started already
+	 *
+	 * @param roleName Name of the role the component fills.
+	 * @return instance of the component
+	 * @throws ComponentException generic exception
+	 */
+	protected Object lookup(String roleName) throws ComponentException 
+	{
+		if (container == null) {
+			if (containerType.equals(CONTAINER_ECM)) 
+			{
+				container = new ECMContainer();
+			} 
+			else 
+			{
+				container = new YAAFIContainer(logLevel);
+			}
+			container.startup(getConfigurationFileName(), getRoleFileName(), getParameterFileName());
+		}
+		return container.lookup(roleName);
+	}
 
-    /**
-     * Releases the component.
-     *
-     * @param component component to be released
-     */
-    protected void release(Object component)
-    {
-        if (container != null)
-        {
-            container.release(component);
-        }
-    }
+	/**
+	 * Helper method for converting to and from Merlin Unit TestCase.
+	 * 
+	 * @param roleName the role name to resolve
+	 * @return the component matching the role
+	 * @throws ComponentException generic exception
+	 */
+	protected Object resolve(String roleName) throws ComponentException 
+	{
+		return lookup(roleName);
+	}
+
+	/**
+	 * Releases the component.
+	 *
+	 * @param component component to be released
+	 */
+	protected void release(Object component) 
+	{
+		if (container != null) 
+		{
+			container.release(component);
+		}
+	}
 }
